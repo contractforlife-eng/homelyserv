1f04d630064187001493af97f8ec3b8f86d0cb62
Emad Abdelhalem <contractforlife@gmail.com>
2026-06-21 10:37:16 +0300

    Fix Prisma for serverless

--- backend/src/utils/prisma.js
+++ backend/src/utils/prisma.js
@@ -1,7 +1,11 @@
 const { PrismaClient } = require('@prisma/client');
 
-const prisma = new PrismaClient({
-  datasourceUrl: "postgresql://postgres.uyzyxmrkdleywpogtpfs:Killuemad-123@aws-0-eu-west-1.pooler.supabase.com:5432/postgres"
-});
+const globalForPrisma = global;
+
+const prisma = globalForPrisma.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== 'production') {
+  globalForPrisma.prisma = prisma;
+}
 
 module.exports = prisma;
