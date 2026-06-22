1a12a66ce7c8d10d1b6378308987f76099cabaa6
Emad Abdelhalem <contractforlife@gmail.com>
2026-06-22 21:09:10 +0300

    Add Bitcoin and USDC crypto payment options

--- backend/src/routes/hires.js
+++ backend/src/routes/hires.js
@@ -5,23 +5,14 @@ const {
   sendOffer, 
   getMyHires, 
   confirmPayment, 
-  submitPayment,
+  submitPayment, 
   getAllHires 
 } = require('../controllers/hireController');
 
-// Send a job offer
 router.post('/', auth, sendOffer);
-
-// Get all hires for current user
-router.get('/my', auth, getMyHires);
-
-// Get all hires (admin only)
 router.get('/all', auth, getAllHires);
-
-// Submit payment for a hire
+router.get('/my', auth, getMyHires);
 router.put('/:id/payment', auth, submitPayment);
-
-// Confirm payment (admin only)
 router.put('/:id/confirm-payment', auth, confirmPayment);
 
 module.exports = router;
