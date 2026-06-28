9c5a196bf3ee45cbabcc9e684f577aaf76bcdf12
Emad Abdelhalem <contractforlife@gmail.com>
2026-06-28 12:56:41 +0300

    Restore working Login page design

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './pages/Login';
-import Dashboard from './pages/Dashboard';
-import MobileDashboard from './pages/MobileDashboard';
 
 function App() {
   return (
@@ -10,7 +8,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Navigate to="/login" />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
         <Route path="*" element={<Navigate to="/login" />} />
       </Routes>
     </BrowserRouter>
