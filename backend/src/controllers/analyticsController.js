c4f6a03e4e02d159746bca744b19d23d4af797bb
contractforlife-eng <contractforlife@gmail.com>
2026-08-27 18:48:23 +0300

    Release Android v1.0.10

--- backend/src/controllers/analyticsController.js
+++ backend/src/controllers/analyticsController.js
@@ -82,7 +82,7 @@ export const recordAnalyticsEvent = async (req, res) => {
 export const redirectToApk = async (req, res) => {
   const source = req.query?.source === 'android' ? 'android' : 'web';
   const dedupeKey = req.query?.dedupeKey;
-  const target = 'https://github.com/contractforlife-eng/homelyserv/releases/download/android-v1.0.9/HomelyServ-1.0.9-10.apk';
+  const target = 'https://github.com/contractforlife-eng/homelyserv/releases/download/android-v1.0.10/HomelyServ-1.0.10-11.apk';
 
   if (isValidDedupeKey(dedupeKey)) {
     try {
