0299919225dd7103e54226797f43c1def16941d3
contractforlife-eng <contractforlife@gmail.com>
2026-08-27 15:19:00 +0300

    Release Android v1.0.8

--- frontend/src/pages/Download.jsx
+++ frontend/src/pages/Download.jsx
@@ -8,19 +8,19 @@ import { classifyDevice } from '../utils/deviceType';
 import { getAnalyticsSource, getOperationalAnalyticsDedupeKey } from '../utils/operationalAnalytics';
 import { API_BASE } from '../config/api';
 
-const RELEASE_MANIFEST_URL = '/downloads/android/1.0.7-8/release.json';
-const FALLBACK_DOWNLOAD_URL = 'https://github.com/contractforlife-eng/homelyserv/releases/download/android-v1.0.7/HomelyServ-1.0.7-8.apk';
+const RELEASE_MANIFEST_URL = '/downloads/android/1.0.8-9/release.json';
+const FALLBACK_DOWNLOAD_URL = 'https://github.com/contractforlife-eng/homelyserv/releases/download/android-v1.0.8/HomelyServ-1.0.8-9.apk';
 const FALLBACK_RELEASE = {
   appName: 'HomelyServ',
-  versionName: '1.0.7',
-  versionCode: 8,
-  releaseDate: '2026-08-24',
-  sizeBytes: 7786795,
+  versionName: '1.0.8',
+  versionCode: 9,
+  releaseDate: '2026-08-27',
+  sizeBytes: 7786472,
   minAndroid: 'Android 7.0',
   minSdk: 24,
   targetSdk: 36,
   packageName: 'com.homelyserv.app',
-  sha256: '8395A94B4787A6535ABA0F4CA22A4F61DF1423FEEBEAEAF55B0007865A1C64DF',
+  sha256: '0AF0FA012BCD84136BABBF7FDE6CD049BB3B8D34CD647D9A84E45B536CB39D20',
   signingCertificateSha256: 'b52c2b1a9d4510993219b60b138530583e4ca29274cc32690f44f2f233674c50',
   downloadUrl: FALLBACK_DOWNLOAD_URL
 };
